9d5d83f2423efee91aa3707197a2d91af1c56ad1
miaraylight <miaraylight@gmail.com>
2025-10-23 13:13:13 -0500

    add hit logic

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -12,8 +12,33 @@ public class App {
         Deck deck = new Deck();
         deck.shuffle();
 
-        // deal 2 cards to each hand
-        for (int i = 0; i < 2; i++) {
+        dealCards(hands, deck, 2);
+
+        displayResults(hands);
+
+        // ask for hit
+        hit(hands, deck);
+
+        defineWinner(hands);
+    }
+
+    private static void hit(ArrayList<Hand> hands, Deck deck) {
+        for (Hand hand: hands) {
+            System.out.println(hand.getName());
+            String answer = input("Hit? Y-yes N-No");
+
+            if (answer.equals("Y")) {
+                // get a card from the deck
+                Card card = deck.deal();
+                // deal that card to the hand
+                hand.deal(card);
+            }
+        }
+    }
+
+    private static void dealCards(ArrayList<Hand> hands, Deck deck, int cardAmount) {
+
+        for (int i = 1; i <= cardAmount; i++) {
             for (Hand hand: hands){
                 // get a card from the deck
                 Card card = deck.deal();
@@ -21,38 +46,33 @@ public class App {
                 hand.deal(card);
             }
         }
+    }
 
-
+    private static void displayResults(ArrayList<Hand> hands) {
         for (Hand hand: hands) {
             String name = hand.getName();
             int handValue = hand.getValue();
             String info = String.format("%s hand is worth %d",name, handValue);
             System.out.println(info);
         }
+    }
 
+    private static Hand defineWinner(ArrayList<Hand> hands) {
         Hand winningHand = null;
-        int bestValue = 0;
-//
-//        for (int i = 0; i < hands.size(); i++) {
-//            int playerValue = hands.get(i).getValue();
-//
-//            // Only consider hands with value <= 21
-//            if (playerValue <= 21 && playerValue > bestValue) {
-//                bestValue = playerValue;
-//                winningHand = hands.get(i);
-//            }
-//        }
+        int highestValue = 0;
 
         for (Hand hand: hands) {
             int handValue = hand.getValue();
 
-            if (handValue <= 21 && handValue > bestValue) {
-                bestValue = handValue;
+            if (handValue <= 21 && handValue > highestValue) {
+                highestValue = handValue;
                 winningHand = hand;
             }
         }
 
         System.out.println("Winner is: " + winningHand.getName() + " with hand: " + winningHand.getValue());
+
+        return winningHand;
     }
 
     public static ArrayList<Hand> getHands() {
